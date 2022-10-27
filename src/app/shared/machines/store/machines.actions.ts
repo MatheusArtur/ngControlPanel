66729173eb9b7a6aac0d041e544dc9b699568bd7
Matheus Artur <matheuscabral@protonmail.com>
2022-10-27 00:47:39 -0300

    rename enum file

--- src/app/shared/machines/store/machines.actions.ts
+++ src/app/shared/machines/store/machines.actions.ts
@@ -1,6 +1,6 @@
 import { createAction, props } from '@ngrx/store';
 import { MachineResponseInterface } from 'src/app/shared/machines/interfaces/machines.interfaces';
-import { MachineActionTypes } from 'src/app/shared/machines/store/machines.types';
+import { MachineActionTypes } from 'src/app/shared/machines/store/machines.enums';
 
 export const getMachinesAction = createAction(MachineActionTypes.GET_MACHINES);
 
